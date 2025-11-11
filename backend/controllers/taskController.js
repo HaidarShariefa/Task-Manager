352c7fd7ea40e9030b8892938a1fe58695f9c38d
Haidar <haidarshariefa33451@gmail.com>
2025-11-12 00:07:59 +0200

    implemented getDasboardData in taskController.js

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -268,6 +268,67 @@ async function updateTaskChecklist(req, res) {
 // @access Private (Admin Only)
 async function getDasboardData(req, res) {
   try {
+    const totalTasks = await Task.countDocuments();
+    const completedTasks = await Task.countDocuments({ status: "Completed" });
+    const pendingTasks = await Task.countDocuments({ status: "Pending" });
+    const overdueTasks = await Task.countDocuments({
+      status: { $ne: "Completed" },
+      dueDate: { $lt: new Date() },
+    });
+
+    // Ensure all possible statuses are included
+    const taskStatuses = ["Pending", "In Progress", "Completed"];
+    const taskDistributionRaw = await Task.aggregate([
+      {
+        $group: {
+          _id: "$status",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    const taskDistribution = taskStatuses.reduce((acc, status) => {
+      const formattedKey = status.replace(/\s+/g, ""); // Remove spaces for response
+      acc[formattedKey] =
+        taskDistributionRaw.find((item) => item._d === status)?.count || 0;
+      return acc;
+    }, {});
+    taskDistribution["All"] = totalTasks; // Add total count to taskDistribution
+
+    //Ensure all priority levels are included
+    const taskPriorities = ["Low", "Medium", "High"];
+    const taskPriorityLevelsRaw = await Task.aggregate([
+      {
+        $group: {
+          _id: "$priority",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+    const taskPriorityLevels = taskPriorities.reduce((acc, priority) => {
+      acc[priority] =
+        taskPriorityLevelsRaw.find((item) => item._id === priority)?.count || 0;
+      return acc;
+    }, {});
+
+    // Fetch recent 10 tasks
+    const recentTasks = await Task.find()
+      .sort({ createdAt: -1 })
+      .limit(10)
+      .select("title status priority dueDate createdAt");
+
+    res.status(200).json({
+      statistics: {
+        totalTasks,
+        completedTasks,
+        pendingTasks,
+        overdueTasks,
+      },
+      charts: {
+        taskDistribution,
+        taskPriorityLevels,
+      },
+      recentTasks,
+    });
   } catch (err) {
     res.status(500).json({ message: "Server Error", error: err.message });
   }
