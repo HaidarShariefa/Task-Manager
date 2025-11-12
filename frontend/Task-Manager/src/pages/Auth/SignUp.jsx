da2f2bcc8d797734313ef1a99f231a09f6969bb9
Haidar <haidarshariefa33451@gmail.com>
2025-11-12 14:45:03 +0200

    implement ProfilePhotoSelector.jsx

--- frontend/Task-Manager/src/pages/Auth/SignUp.jsx
+++ frontend/Task-Manager/src/pages/Auth/SignUp.jsx
@@ -1,3 +1,54 @@
-export default function SignUp(){
-    return <div>SignUp</div>
+import { useState } from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { validateEmail } from "../../utils/helper";
+import ProfilePhotoSelector from "../../components/Inputs/ProfilePhotoSelector";
+
+export default function SignUp() {
+  const [profilePic, setProfilePic] = useState(null);
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [adminInviteToken, setAdminInviteToken] = useState("");
+
+  const [error, setError] = useState(null);
+
+  // Handle signup form submission
+  async function handleSignUp(e) {
+    e.preventDefault();
+
+    if (!fullName) {
+      setError("Please enter your full name");
+      return;
+    }
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter a password");
+      return;
+    }
+
+    setError("");
+
+    // Signup api call
+  }
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-full h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black ">Create an account</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Join us today by entering your details below.
+        </p>
+
+        <form onSubmit={handleSignUp}>
+          <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4"></div>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 }
