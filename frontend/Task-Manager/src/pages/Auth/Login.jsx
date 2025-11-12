ec02ebf485d7207d31d4b1f4de2ba0e8128cb2de
Haidar <haidarshariefa33451@gmail.com>
2025-11-12 13:29:13 +0200

    implement login form UI in Login.jsx

--- frontend/Task-Manager/src/pages/Auth/Login.jsx
+++ frontend/Task-Manager/src/pages/Auth/Login.jsx
@@ -1,3 +1,73 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
+
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  // Handle login form submission
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter a password");
+      return;
+    }
+
+    setError("");
+
+    // Login api call
+  }
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your details to login
+        </p>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            label="Email Address"
+            placeholder="john@example.com"
+            type="text"
+          />
+          <Input
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            label="Password"
+            placeholder="Min. 8 Characters"
+            type="password"
+          />
+
+          {error && <p className="text-red-500 text-xs pb-2.5 ">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            LOGIN
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don't have an account? {""}
+            <Link className="font-medium text-blue-700 underline" to="/signup">
+              Sign Up
+            </Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
+  );
 }
