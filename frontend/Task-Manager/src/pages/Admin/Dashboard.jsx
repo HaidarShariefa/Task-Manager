8954db818ab0dfb090e279cad3246ff8c5dae51e
Haidar <haidarshariefa33451@gmail.com>
2025-11-15 01:42:03 +0200

    implement SideMenu Navbar and some of Dashboard

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -1,10 +1,54 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import { UserContext } from "../../context/userContext";
+import DashboardLayout from "../../components/layouts/DashboardLayout";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import moment from "moment";
 
 export default function Dashboard() {
   useUserAuth();
 
   const { user } = useContext(UserContext);
-  return <div>Admin Dashboard</div>;
+
+  const navigate = useNavigate();
+
+  const [dashboardData, setDashboardData] = useState(null);
+  const [pieChartData, setPieChartData] = useState([]);
+  const [barChartData, setBarChartData] = useState([]);
+
+  async function getDasboardData() {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_DASHBOARD_DATA
+      );
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (err) {
+      console.error("Error fetching users:", err);
+    }
+  }
+
+  useEffect(() => {
+    getDasboardData();
+
+    return () => {};
+  }, []);
+
+  return (
+    <DashboardLayout activeMenu="Dashboard">
+      <div className="card my-5">
+        <div>
+          <div className="col-span-3">
+            <h2 className="text-xl md:text-2xl">Hello! {user?.name}</h2>
+            <p className="text-xs md:text-[13px] text-gray-400 mt-1.5">
+              {moment().format("dddd Do MMM YYYY")}
+            </p>
+          </div>
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 }
