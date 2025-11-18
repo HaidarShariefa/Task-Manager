a5dfb7f8fe7b588a58ca0e0846d719a3cec43200
Haidar <haidarshariefa33451@gmail.com>
2025-11-19 00:40:13 +0200

    add addAttachmentsInput: style attachment items and buttons

--- frontend/Task-Manager/src/components/Inputs/AddAttachmentsInput.jsx
+++ frontend/Task-Manager/src/components/Inputs/AddAttachmentsInput.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { HiMiniPlus, HiOutlineTrash } from "react-icons/hi2";
 import { LuPaperclip } from "react-icons/lu";
 
-export default function AddAttachmentsInput(attachments, setAttachments) {
+export default function AddAttachmentsInput({ attachments, setAttachments }) {
   const [option, setOption] = useState("");
 
   // Function to handle adding an option
@@ -22,33 +22,39 @@ export default function AddAttachmentsInput(attachments, setAttachments) {
   return (
     <div>
       {attachments.map((attachment, index) => (
-        <div key={attachment} className="">
-          <div className="">
-            <LuPaperclip className="" />
-            <p className="">{item}</p>
+        <div
+          key={attachment}
+          className="flex justify-between bg-gray-50 border border-gray-100 px-3 py-2 rounded-md mb-3 mt-2"
+        >
+          <div className="flex-1 flex items-center gap-3 border border-gray-100">
+            <LuPaperclip className="text-gray-400" />
+            <p className="text-xs text-black">{attachment}</p>
           </div>
 
-          <button className="" onClick={() => handleDeleteOption(index)}>
-            <HiOutlineTrash className="" />
+          <button
+            className="cursor-pointer"
+            onClick={() => handleDeleteOption(index)}
+          >
+            <HiOutlineTrash className="text-lg text-red-500" />
           </button>
         </div>
       ))}
 
-      <div className="">
-        <div className="">
-          <LuPaperclip className="" />
+      <div className="flex items-center gap-5 mt-4">
+        <div className="flex-1 flex items-center gap-3 border border-gray-100 rounded-md px-3">
+          <LuPaperclip className="text-gray-400" />
 
           <input
             type="text"
             placeholder="Add File Link"
             value={option}
             onChange={(target) => setOption(target.value)}
-            className=""
+            className="w-full text-[13px] text-black outline-none bg-white py-2"
           />
         </div>
 
-        <button className="" onClick={handleAddOption}>
-          <HiMiniPlus className="" /> Add
+        <button className="card-btn text-nowrap" onClick={handleAddOption}>
+          <HiMiniPlus className="text-lg" /> Add
         </button>
       </div>
     </div>
