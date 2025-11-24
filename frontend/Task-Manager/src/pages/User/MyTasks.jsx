c85baeec77dc97daf7c268af5d0f89bf1aa721e5
Haidar <haidarshariefa33451@gmail.com>
2025-11-25 00:36:33 +0200

    implement MyTasks page

--- frontend/Task-Manager/src/pages/User/MyTasks.jsx
+++ frontend/Task-Manager/src/pages/User/MyTasks.jsx
@@ -1,3 +1,88 @@
+import { useEffect, useState } from "react";
+import DashboardLayout from "../../components/Layouts/DashboardLayout";
+import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
+import { LuFileSpreadsheet } from "react-icons/lu";
+import TaskStatusTabs from "../../components/TaskStatusTabs";
+import TaskCard from "../../components/Cards/TaskCard";
+
 export default function MyTasks() {
-  return <div>My Tasks</div>;
+  const [allTasks, setAllTasks] = useState([]);
+  const [tabs, setTabs] = useState([]);
+  const [filterStatus, setFilterStatus] = useState("All");
+
+  const navigate = useNavigate();
+
+  async function getAllTasks() {
+    try {
+      const response = await axiosInstance.get(API_PATHS.TASKS.GET_ALL_TASKS, {
+        params: {
+          status: filterStatus === "All" ? "" : filterStatus,
+        },
+      });
+      setAllTasks(response.data?.tasks?.length > 0 ? response.data.tasks : []);
+
+      // Map statusSummary data with fixed label and order
+      const statusSummary = response.data?.statusSummary || {};
+
+      const statusArray = [
+        { label: "All", count: statusSummary.all || 0 },
+        { label: "Pending", count: statusSummary.pendingTasks || 0 },
+        { label: "In Progress", count: statusSummary.inProgressTasks || 0 },
+        { label: "Completed", count: statusSummary.completedTasks || 0 },
+      ];
+
+      setTabs(statusArray);
+    } catch (err) {
+      console.error("Error fetching tasks:", err);
+    }
+  }
+
+  function handleClick(taskId) {
+    navigate(`/user/task-details/${taskId}`);
+  }
+
+  useEffect(() => {
+    getAllTasks(filterStatus);
+    return () => {};
+  }, [filterStatus]);
+
+  return (
+    <DashboardLayout activeMenu="My Tasks">
+      <div className="my-5">
+        <div className="flex flex-col lg:flex-row lg:items-center justify-between">
+          <h2 className="text-xl md:text-xl font-medium">My Tasks</h2>
+
+          {tabs?.[0]?.count > 0 && (
+            <TaskStatusTabs
+              tabs={tabs}
+              activeTab={filterStatus}
+              setActiveTab={setFilterStatus}
+            />
+          )}
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+          {allTasks.map((item, index) => (
+            <TaskCard
+              key={item._id}
+              title={item.title}
+              description={item.description}
+              priority={item.priority}
+              progress={item.progress}
+              status={item.status}
+              createdAt={item.createdAt}
+              dueDate={item.dueDate}
+              assignedTo={item.assignedTo?.map((item) => item.profileImgUrl)}
+              attachmentCount={item.attachments?.length || 0}
+              completedTodoCount={item.completedTodoCount || 0}
+              todoChecklist={item.todoChecklist || []}
+              onClick={() => handleClick(item._id)}
+            />
+          ))}
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 }
