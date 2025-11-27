1709f5210217f8749b90287cdbe0387c006c7dd1
Haidar <haidarshariefa33451@gmail.com>
2025-11-27 22:35:28 +0200

    edit server.js

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,10 @@ const app = express();
 //Middleware to handle CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 
